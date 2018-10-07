44ae66abc63189f7c39c763cef07b4df76ac5ba8
Shivam Singhal <shivamsinghal283@gmail.com>
2018-10-07 16:05:59 +0530

    saved successfully

---
+++ src/main/java/javaprojects/myfirstmavenproject/App.java
@@ -0,0 +1,13 @@
+package javaprojects.myfirstmavenproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
